b03639b77cd4cceabed46e5ae03f449c8e16af0d
Tim Vollmer <timjvollmer@gmail.com>
2012-02-08 20:28:56 -0600

    corrected a bug in the BootStrap file.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,11 +6,11 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        def ipAddrs = [] << new IpAddress(ipAddr:'10.0.0.1')  << new IpAddress(ipAddr:'10.0.0.2')
+        def ipAddrs = [] << new IpAddress(ipAddr:'10.0.0.1') << new IpAddress(ipAddr:'10.0.0.2')
         println "ipAddrs:$ipAddrs"
-        def custOrder = new CustomerOrder(customerName:'Foo Customer',ipAddesses:ipAddrs).save(flush:true)
+        def custOrder = new CustomerOrder(customerName:'Foo Customer',ipAddresses:ipAddrs).save(flush:true)
         println "$custOrder"
-
+        
     }
     def destroy = {
     }
